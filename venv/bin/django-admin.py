1c845b8babd0b8b4e500990aebc926cf5697620d
melihkarakoc1379 <melihkarakoc1881@gmail.com>
2020-04-21 02:02:53 +0300

    removed old virtualenv added updated one

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/melih/Masaüstü/web-kütüphane/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ vlibrary/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/melih/Envs/vlibrary/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
